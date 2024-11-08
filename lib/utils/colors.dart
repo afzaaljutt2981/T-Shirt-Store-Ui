e78db9e407a9a7e20dc84d00e7877069ae356242
Muhammad Afzaal <118747946+afzaaljutt2981@users.noreply.github.com>
2024-11-08 13:08:39 +0500

    Update colors.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -5,6 +5,6 @@ class AppColors {
   static const Color secondaryColor = Color(0xff55544a);
   static const Color tertiaryColor = Color(0xffD2D2d3);
   static const Color blueShade = Color(0xff6f95b3);
-  static const Color blueShade1 = Color(0xff9db8ca);
+  static const Color blueShade1 = Color(0xff9db8ca); 
   static const Color buttonColor = Colors.amber;
 }
