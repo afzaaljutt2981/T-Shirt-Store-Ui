142886e2cc3f575de62062f365eec5e10183b266
Muhammad Afzaal <118747946+afzaaljutt2981@users.noreply.github.com>
2025-03-09 15:30:02 +0500

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( 
       appBar: AppBar(
         backgroundColor: AppColors.primaryColor,
         elevation: 0,
