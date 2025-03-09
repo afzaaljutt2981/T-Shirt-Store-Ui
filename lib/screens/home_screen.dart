d5141ec5dc21d38ca13a10ba90f7107fc70ce1ff
Muhammad Afzaal <118747946+afzaaljutt2981@users.noreply.github.com>
2025-03-09 15:29:55 +0500

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ import '../widgets/product_view.dart';
  
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
