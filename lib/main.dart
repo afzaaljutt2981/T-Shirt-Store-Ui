6ed6b6c5eb38389c4dde2d9567584a21a1090f20
Muhammad Afzaal <118747946+afzaaljutt2981@users.noreply.github.com>
2025-03-10 23:49:17 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:tshirtui/utils/colors.dart';
 import 'screens/nav_bar.dart';
 
 void main() { 
-  runApp(const MyApp());
+  runApp(const MyApp()); 
 }   
 
 class MyApp extends StatelessWidget {
